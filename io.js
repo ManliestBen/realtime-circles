2798d0cc86958e12ed629c0b79195e553a8cbcc4
ManliestBen <benjamintmanley@gmail.com>
2019-09-23 16:56:08 -0500

    deployment

--- io.js
+++ io.js
@@ -1,12 +1,30 @@
 var io = require('socket.io')();
- 
-// Listen for new connections from clients (socket)
+
+// object to track player's initials
+var players = {};
+
 io.on('connection', function (socket) {
-    //new code below
+
+    socket.on('register-player', function (initials) {
+      // each socket has a unique id
+      players[socket.id] = initials;
+      io.emit('update-player-list', Object.keys(players).map(id => players[id]));
+    });
+
     socket.on('add-circle', function (data) {
       io.emit('add-circle', data);
     });
-  });
 
-// io represents socket.io on the server - let's export it
+    socket.on('clear-display', function () {
+      io.emit('clear-display');
+    });
+
+    // when the player disconnects, remove key & notify clients
+    socket.on('disconnect', function () {
+      delete players[socket.id];
+      io.emit('update-player-list', Object.keys(players).map(id => players[id]));
+    });
+
+});
+
 module.exports = io;
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,40 +1,56 @@
+// get our connection to the socket.io server
 var socket = io();
-
+// listen to the server for the `add-circle` event
 socket.on('add-circle', function (data) {
-  console.log(data);
+  // console.log(data);
   addCircle(data);
 });
 
-console.log(socket);
+// listen to the server for the `clear-display` event
+socket.on('clear-display', function () {
+  circles.innerHTML = '';
+});
+
+// listen to the server for when the player list has changed
+socket.on('update-player-list', function (data) {
+  var playerList = '<li>' + data.join('</li><li>') + '</li>';
+  players.innerHTML = playerList;
+});
 
 var circles = document.getElementById('circles');
+var players = document.getElementById('players');
 var initials = '';
 
 circles.addEventListener('click', function(evt) {
-  // replace current line of code with this code
+  //addCircle(evt.clientX, evt.clientY, randomBetween(10,100), getRandomRGBA());
+  
   socket.emit('add-circle', {
     initials: initials,
     x: evt.clientX,
     y: evt.clientY,
-    dia: randomBetween(10,100),
+    dia: randomBetween(10,125),
     rgba: getRandomRGBA()
   });
 });
 
 document.getElementsByTagName('button')[0].addEventListener('click', function() {
-  circles.innerHTML = '';
+  socket.emit('clear-display');
 });
 
 do {
   initials = getInitials();
 } while (initials.length < 2 || initials.length > 3);
+// new code below
+socket.emit('register-player', initials);
 
 function getInitials() {
   var input = prompt("Please enter your initials");
   return input ? input.toUpperCase() : '';
 }
 
-function addCircle({x, y, dia, rgba, initials}) {
+// was: function addCircle(x, y, dia, rgba)
+// now using ES2015 destructuring assignment
+function addCircle( {x, y, dia, rgba, initials} ) {
   var el = document.createElement('div');
   el.style.left = x - Math.floor(dia / 2 + 0.5) + 'px';
   el.style.top = y - Math.floor(dia / 2 + 0.5) + 'px';
